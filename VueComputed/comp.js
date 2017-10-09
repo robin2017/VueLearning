b85091d7f527ec1a30a286e33546a7ea080bded8
robin2017 <robin.seu@foxmail.com>
2017-10-09 21:53:53 +0800

    last commit

---
+++ VueComputed/comp.js
@@ -0,0 +1,22 @@
+/**
+ * Created by robin on 2017/10/3.
+ */
+var vm = new Vue({
+    el: '#example',
+    data: {
+        message: 'Hello'
+    },
+    computed: {
+        reversedMessage: function () {
+            return this.message.split('').reverse().join('');
+        },
+        user() {
+            return this.message.split('').reverse().join('');
+        }
+    },
+    methods: {
+        reversedMessageM: function () {
+            return this.message.split('').reverse().join('');
+        }
+    }
+});
